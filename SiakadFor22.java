b6d0654e10e79f57898c609fe8c944ad4705bf73
tiara <haiiiinirara@gmail.com>
2025-11-05 21:02:48 +0700

    feat: added modif

--- SiakadFor22.java
+++ SiakadFor22.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class SiakadFor22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-       double grade, highest = 0, lowest = 100;
-       for (int i = 1; i <= 10; i++){
+        double grade = 0, highest = 0, lowest = 100;
+        int passCount = 0, failCount = 0;
+        String status;
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Input grade for student no- " + i + ": ");
             grade = scan.nextDouble();
             if (grade > highest) {
@@ -13,8 +15,18 @@ public class SiakadFor22 {
             if (grade < lowest) {
                 lowest = grade;
             }
-       }
-       System.out.println("Highest grade is: " + highest);
-       System.out.println("Lowest grade is: " + lowest);
+
+              if (grade >= 60) {
+                passCount++;
+            } else {
+                failCount++;
+            }
+        }
+       
+        System.out.println("Student passed");
+        System.out.println("Highest grade is: " + highest);
+        System.out.println("Lowest grade is: " + lowest);
+        System.out.println("Number of students passed: " + passCount);
+        System.out.println("Number of students failed: " + failCount);
     }
 }
